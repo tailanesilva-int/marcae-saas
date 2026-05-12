fea652696ff4aad693711ff8373426fdbe7d1379
Nane <tailanessilva93@gmail.com>
2026-05-11 21:14:18 -0300

    Corrige import prisma painel master

--- app/api/master/empresas/route.ts
+++ app/api/master/empresas/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { prisma } from "@/lib/prisma";
+import { prisma } from "@/app/lib/prisma";
 
 function gerarSlug(texto: string) {
   return texto
